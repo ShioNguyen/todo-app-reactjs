27f0bef8c18dcca055c85284ed30fd300b4544df
ShioNguyen <ndnghi.coder@gmail.com>
2021-02-08 14:26:12 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Form, FormGroup, Input, Button, Container, Col, Row } from 'reactstrap'
+import {BsCheck} from 'react-icons/bs';
+
+function getWindowDimensions() {
+  const { innerWidth } = window;
+  return innerWidth;
+}
 
 function App() {
+  
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [btnBlock, setBtnBlock] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      const {innerWidth} = window;
+      setWindowDimensions(innerWidth);
+    })
+    if(windowDimensions < 576) {
+      setBtnBlock(true);
+    }
+    else {
+      setBtnBlock(false);
+    }
+  }, [windowDimensions])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Container 
+        style={{
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Form style={{width: '50%'}}>
+          <FormGroup row>
+            <Col sm={10} xs={12}>
+              <Input placeholder='make your plan'/>
+            </Col>
+            <Col sm={2} xs={12}>
+              <Button block={btnBlock} color='primary'>Submit</Button>
+            </Col>
+          </FormGroup>
+          <Row >
+            <Col xs={12}>
+              <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                backgroundColor: '#fff',
+                padding: '10px',
+                minHeight: '30px',
+              }}>
+                <h5>Hello</h5>
+                <Button size='sm' color='warning'><BsCheck/></Button>
+              </div>
+            </Col>
+          </Row>
+        </Form>
+
+      </Container>
+    </>    
+  )
 }
 
 export default App;
