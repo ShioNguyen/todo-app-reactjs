a11e167fecbc9fded85c80e31487a0ba7e2d3131
ShioNguyen <ndnghi.coder@gmail.com>
2021-02-09 19:37:03 +0700

    update ui

--- src/components/styles.jsx
+++ src/components/styles.jsx
@@ -8,7 +8,6 @@ module.exports = {
     },
 
     header : {
-        zIndex: 99999,
         position: 'fixed', 
         width: '100vw',
         boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
