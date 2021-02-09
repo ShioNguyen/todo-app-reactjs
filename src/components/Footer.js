385e7958771df3b61e8eee29805e2b55b8f178d4
ShioNguyen <ndnghi.coder@gmail.com>
2021-02-09 19:49:41 +0700

    update ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import {footer, footerTtitle} from './styles';
 export default function Footer() {
     return (
         <div style={footer}>
+            <hr/>
             <p style={footerTtitle}>&copy; ShioNguyen</p>
         </div>
     )
--- src/components/styles.jsx
+++ src/components/styles.jsx
@@ -17,7 +17,6 @@ module.exports = {
         position: 'fixed', 
         width: '100vw', 
         bottom: '0',
-        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
     },
 
     footerTtitle :{
